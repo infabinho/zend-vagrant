858a3c5681c565b26a817f30c66df8a3f81a05af
Antonio Spinelli <antonio.spinelli@westwing.com.br>
2013-04-20 11:16:54 -0300

    add zend create project in puppet

--- vagrant/puppet/app/php.pp
+++ vagrant/puppet/app/php.pp
@@ -27,34 +27,37 @@ class app::php {
 #        require => Class["php::install", "php::config"],
 #    }
 #
-    exec { 'install_composer':
-        command => 'curl -s https://getcomposer.org/installer | php -- --install-dir="/bin"',
-        require => [ Package['curl'], Class["php::install", "php::config"] ],
-        unless  => 'which composer.phar',
-    }
+#    exec { 'install_composer':
+#        command => 'curl -s https://getcomposer.org/installer | php -- --install-dir="/bin"',
+#        require => [ Package['curl'], Class["php::install", "php::config"] ],
+#        unless  => 'which composer.phar',
+#    }
+#
+#    class { 'pear': require => Class['php::install'] }
+#    class { 'phpqatools': require => Class['pear'] }
+#
+#    pear::package { "PHPUnit_MockObject":
+#        repository => "pear.phpunit.de",
+#        require    => Pear::Package["PEAR"],
+#    }
+#
+#    pear::package { "PHP_CodeCoverage":
+#        repository => "pear.phpunit.de",
+#        require    => Pear::Package["PEAR"],
+#    }
+#
+#    pear::package { "PHPUnit_Selenium":
+#        repository => "pear.phpunit.de",
+#        require    => Pear::Package["PEAR"],
+#    }
+#
+#    pear::package { "DocBlox":
+#        version => 'latest',
+#        repository => "pear.docblox-project.org"
+#    }
 
-    class { 'pear': require => Class['php::install'] }
-    class { 'phpqatools': require => Class['pear'] }
 
-    pear::package { "PHPUnit_MockObject":
-        repository => "pear.phpunit.de",
-        require    => Pear::Package["PEAR"],
-    }
-
-    pear::package { "PHP_CodeCoverage":
-        repository => "pear.phpunit.de",
-        require    => Pear::Package["PEAR"],
-    }
-
-    pear::package { "PHPUnit_Selenium":
-        repository => "pear.phpunit.de",
-        require    => Pear::Package["PEAR"],
-    }
-
-    pear::package { "DocBlox":
-        version => 'latest',
-        repository => "pear.docblox-project.org"
-    }
+    include app::zend
 
     if 'nginx' == $webserver {
         include app::php::fpm
