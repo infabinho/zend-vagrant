abf276436057d72f2b3a5ac5cd67b196b86f0d1f
Antonio Spinelli <antonio.spinelli@westwing.com.br>
2013-04-28 14:28:04 -0300

    zend enable form and layout

--- vagrant/puppet/app/zend.pp
+++ vagrant/puppet/app/zend.pp
@@ -5,4 +5,14 @@ class app::zend {
         command => "/bin/bash -c 'cd /srv/www/vhosts && ./$vhost.dev/vendor/zendframework/zendframework1/bin/zf.sh create project $vhost.dev'",
     }
 
+    exec {"enable-form":
+        require => Exec["create-project"],
+        command => "/bin/bash -c 'cd /srv/www/vhosts/$vhost.dev && ./vendor/zendframework/zendframework1/bin/zf.sh enable form'",
+    }
+
+    exec {"enable-layout":
+        require => Exec["create-project"],
+        command => "/bin/bash -c 'cd /srv/www/vhosts/$vhost.dev && ./vendor/zendframework/zendframework1/bin/zf.sh enable layout'",
+    }
+
 }
